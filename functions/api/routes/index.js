ba5a13b8394d14dc7cebb7589503095352e770a1
=Jaeho-Jo <= jojaeho1230@naver.com>
2021-11-25 21:56:46 +0900

    [FEAT]INDEX추가

--- functions/api/routes/index.js
+++ functions/api/routes/index.js
@@ -2,5 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 router.use('/comment', require('./comment'));
+router.use('/webtoon', require('./webtoon'));
 
 module.exports = router;
