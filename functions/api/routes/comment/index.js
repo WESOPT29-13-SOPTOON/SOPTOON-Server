2c80a89c2589201b96307a43c2e6872eeb10e189
=Jaeho-Jo <= jojaeho1230@naver.com>
2021-11-25 23:03:16 +0900

    [FEAT] 댓글작성 라우터 추가

--- functions/api/routes/comment/index.js
+++ functions/api/routes/comment/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/:webtoonId', require('./commentGET'));
+router.post('/:webtoonId', require('./commentPOST'));
 router.get('/:webtoonId/best', require('./bestCommentGET'));
 
 module.exports = router;
